4cac929890b93f578bd5ecbdf0a2c2792075a7ed
Avikshit Banerjee <avikshitbanerjee@gmail.com>
2023-08-15 13:21:18 +0100

    requirements installed

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-summarise-app"
+AUTHOR_USER_NAME = "avikshit-banerjee"
+SRC_REPO = "textSummariser"
+AUTHOR_EMAIL = "avikshitbanerjee@gmail.com"
+
+#This will go through the constructor files in every folder and set it up as a local package
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src") #search for constructor files in src folder
+)
